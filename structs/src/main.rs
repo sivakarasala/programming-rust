74558db2630ee194fa71ff374b367d0cf51f3dfc
SivaKrishna <shivataraya@gmail.com>
2022-11-29 23:48:48 +0530

    more on structs

--- structs/src/main.rs
+++ structs/src/main.rs
@@ -73,6 +73,20 @@ fn main() {
     let mut q = Queue::new();
 
     q.push('*');
+
+    let mut q = Queue::new();
+    let mut r = Queue::new();
+
+    q.push("CAD");  // apparently a Queue<&'static str>
+    r.push(0.74);   // apparently a Queue<f64>
+
+    q.push("BTC");   // Bitcoins per USD, 2019-6
+    r.push(13764.0); // Rust fails to detect irrational exuberance
+
+    let a = [0, -3, 0, 15, 48];
+    let e = find_extrema(&a);
+    assert_eq!(*e.least, -3);
+    assert_eq!(*e.greatest, 48);
 }
 
 struct Broom {
@@ -98,24 +112,24 @@ fn chop(b: Broom) -> (Broom, Broom) {
 
 struct Bounds(usize, usize);
 
-pub struct Queue {
-    older: Vec<char>,
-    younger: Vec<char>
+pub struct Queue<T> {
+    older: Vec<T>,
+    younger: Vec<T>
 }
 
-impl Queue {
+impl<T> Queue<T> {
     // Type-Associated function
-    pub fn new() -> Queue {
-        Queue { older: Vec::new(), younger: Vec::new() }
+    pub fn new() -> Self {
+        Self { older: Vec::new(), younger: Vec::new() }
     }
 
 
     // methods
-    pub fn push(&mut self, c: char) {
-        self.younger.push(c);
+    pub fn push(&mut self, t: T) {
+        self.younger.push(t);
     }
 
-    pub fn pop(&mut self) -> Option<char> {
+    pub fn pop(&mut self) -> Option<T> {
         if self.older.is_empty() {
             if self.younger.is_empty() {
                 return None;
@@ -133,8 +147,14 @@ impl Queue {
         self.older.is_empty() && self.younger.is_empty()
     }
 
-    pub fn split(self) -> (Vec<char>, Vec<char>) {
+    pub fn split(self) -> (Vec<T>, Vec<T>) {
         (self.older, self.younger)
+    }
+}
+
+impl Queue<f64> {
+    fn sum(&self) -> f64 {
+        34.50
     }
 }
 
@@ -149,4 +169,59 @@ impl Vector2 {
 
     const NAME: &'static str = "Vector2";
     const ID: u32 = 18;
+}
+
+struct Extrema<'elt> {
+    greatest: &'elt i32,
+    least: &'elt i32,
+}
+
+fn find_extrema<'s>(slice: &'s [i32]) -> Extrema<'s> {
+    let mut greatest = &slice[0];
+    let mut least = &slice[0];
+
+    for i in 1..slice.len() {
+        if slice[i] < *least { least = &slice[i]; }
+        if slice[i] > *greatest { greatest = &slice[i]; }
+    }
+    Extrema { greatest, least }
+}
+
+struct Polynomial<const N: usize> {
+    coefficients: [f64; N]
+}
+
+impl<const N: usize> Polynomial<N> {
+    fn new(coefficients: [f64; N]) -> Polynomial<N> {
+        Polynomial { coefficients }
+    }
+
+    fn eval(&self, x: f64) -> f64 {
+        let mut sum = 0.0;
+        for i in (0..N).rev() {
+            sum = self.coefficients[i] + x * sum;
+        }
+
+        sum
+    }
+}
+
+#[derive(Copy, Clone, Debug, PartialEq)]
+struct Point {
+    x: f64,
+    y: f64,
+}
+
+pub struct SpiderRobot {
+    species: String,
+    web_enabled: bool,
+    leg_devices: [fd::FileDesc; 8],
+}
+
+use std::rc::Rc;
+
+pub struct SpiderSenses {
+    robot: Rc<SpiderRobot>,
+    eyes: [Camera; 32],
+    motion: Accelerometer,
 }
